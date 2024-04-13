1add6ed0cc2109289ce3f3eb8ce4f187ad6a3dc5
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-04-13 11:03:23 +0530

    toggle navbar

--- src/components/common/Navbar/ResponsiveNav.jsx
+++ src/components/common/Navbar/ResponsiveNav.jsx
@@ -10,71 +10,77 @@ import useOnClickOutside from "../../../hooks/useOnClickOutside";
 
 export default function ResponsiveNav({ loading, subLinks }) {
   const { token } = useSelector((state) => state.auth);
-//   const dispatch = useDispatch()
-//   const navigate = useNavigate()
+  //   const dispatch = useDispatch()
+  //   const navigate = useNavigate()
   const [open, setOpen] = useState(false);
 
-  const ref = useRef(null)
-  useOnClickOutside(ref, () => setOpen(false))
+  const ref = useRef(null);
+  useOnClickOutside(ref, () => setOpen(open));
+
   return (
     <div className="md:hidden relative ">
       <div
-        className="text-richblack-25 text-3xl cursor-pointer transition-all ease-in-out duration-500 "
+        className="text-richblack-25 text-3xl cursor-pointer transition-all ease-in-out duration-200 "
         onClick={() => setOpen(!open)}
       >
-        {!open && <FaBars /> }
+        {!open ? <FaBars /> : <RxCross2 />}
       </div>
-{ open && 
-      <div
-        className={`${
-          open ? "flex flex-col gap-2" : "hidden"
-        } w-[180px] text-richblack-50 absolute z-10 top-[3rem] right-0 border border-richblack-600 bg-richblack-800 p-3 rounded-md items-center transition-all ease-in-out duration-500`}
-        onClick={(e) => e.stopPropagation()}
-        ref={ref}
-      >
-        <div className="w-full flex justify-end text-3xl" onClick={() => setOpen(!open)}>
+      {open && (
+        <div
+          className={`${
+            open ? "flex flex-col gap-2" : "hidden"
+          } w-[180px] text-richblack-50 absolute z-10 top-[3rem] right-0 border border-richblack-600 bg-richblack-800 py-4 rounded-md items-center transition-all ease-in-out duration-200`}
+          onClick={(e) => e.stopPropagation()}
+          ref={ref}
+        >
+          {/* <div className="w-full flex justify-end text-3xl" onClick={() => setOpen(!open)}>
         <RxCross2 />
-        </div>
-        <div className="flex flex-col gap-5 items-center">
-        
-        <div>
-          {token === null && (
-            <div className="flex  gap-2 items-center">
-              <Link to="/login">
-                <button className="border border-richblack-700 bg-richblack-700 px-3 py-2 text-richblack-100 rounded-lg" 
-                onClick={() => setOpen(false)}>
-                  Log in
-                </button>
-              </Link>
-              <Link to="/signup">
-                <button className="border border-richblack-700 bg-richblack-700 px-3 py-2 text-richblack-100 rounded-lg" 
-                onClick={() => setOpen(false)}>
-                  Sign up
-                </button>
-              </Link>
+        </div> */}
+          <div className="flex flex-col gap-5 items-center">
+            <div>
+              {token === null && (
+                <div className="flex  gap-2 items-center">
+                  <Link to="/login">
+                    <button
+                      className="border border-richblack-700 bg-richblack-700 px-3 py-2 text-richblack-100 rounded-lg"
+                      onClick={() => setOpen(false)}
+                    >
+                      Log in
+                    </button>
+                  </Link>
+                  <Link to="/signup">
+                    <button
+                      className="border border-richblack-700 bg-richblack-700 px-3 py-2 text-richblack-100 rounded-lg"
+                      onClick={() => setOpen(false)}
+                    >
+                      Sign up
+                    </button>
+                  </Link>
+                </div>
+              )}
+              {
+                // token?console.log("yes"):console.log("no")
+                <div className=" flex justify-center items-center">
+                  {token && (
+                    <ProfilrDropDown ItemClick={() => setOpen(!open)} />
+                  )}
+                </div>
+              }
             </div>
-          )}
-          {
-            // token?console.log("yes"):console.log("no")
-            <div className=" flex justify-center items-center">
-            {token && <ProfilrDropDown ItemClick={() => setOpen(false)}/>}</div>
-          }
-        </div>
 
-        <div>
-          <NavItem
-            loading={loading}
-            subLinks={subLinks}
-            open={open}
-            customClass1={"flex-col"}
-            customClass2={"block  md:hidden"}
-            ItemClick={() => setOpen(false)}
-            
-          />
+            <div>
+              <NavItem
+                loading={loading}
+                subLinks={subLinks}
+                open={open}
+                customClass1={"flex-col"}
+                customClass2={"block  md:hidden"}
+                ItemClick={() => setOpen(false)}
+              />
+            </div>
+          </div>
         </div>
-        </div>
-      </div>
-}
+      )}
     </div>
   );
 }
