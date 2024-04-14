f2d0d1150392e5162ef1e39cb42c9acc6a4ed050
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-04-14 07:55:10 +0530

    video z-index

--- src/components/core/ViewCourse/VideoDetails.jsx
+++ src/components/core/ViewCourse/VideoDetails.jsx
@@ -178,7 +178,7 @@ export default function VideoDetails() {
                   backgroundImage:
                     "linear-gradient(to top, rgb(0, 0, 0), rgba(0,0,0,0.7), rgba(0,0,0,0.5), rgba(0,0,0,0.1)",
                 }}
-                className="full absolute inset-0 z-[100] grid h-full place-content-center font-inter"
+                className="full absolute inset-0 z-10 grid h-full place-content-center font-inter"
               >
                     {
                         !completedLectures.includes(subSectionId) && (
