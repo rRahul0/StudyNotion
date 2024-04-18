41b80782d48b80f3feb2968aef3734cfd694de87
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-04-18 21:34:01 +0530

    otp problem

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -13,7 +13,7 @@ const otpSchema = new mongoose.Schema({
     createdAt: {  
         type: Date,
         default: Date.now(),
-        expires: 300,
+        expires: 5*60*1000,
     }
 })
 
--- src/components/core/Dashboard/Settings/Settings.jsx
+++ src/components/core/Dashboard/Settings/Settings.jsx
@@ -8,7 +8,7 @@ import UpdatePassword from "./UpdatePassword"
 export default function Settings() {
   return (
     <>
-      <h1 className=" mb-14 text-3xl font-medium text-richblack-5">Edit Profile</h1>
+      <h1 className=" mb-14 text-3xl font-medium text-richblack-5 ">Edit Profile</h1>
       {/* Change Profile Picture */}
       <ChangeProfilePicture />
 
