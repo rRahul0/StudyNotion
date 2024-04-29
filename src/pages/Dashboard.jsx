6c190eb3352ed0c97bad149cbef1d1710f56cea8
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-04-29 12:31:55 +0530

    dashboard width in maxsm

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -26,24 +26,25 @@ export default function Dashboard() {
       <div className="relative  md:relative flex " ref={ref}>
         <div className="hidden md:block ">
           <Sidebar />
-        </div> 
-        
+        </div>
+
         <div className="md:hidden">{open && <Sidebar />}</div>
-        
+
       </div>
 
       {/* modal hidden */}
-      <div className="h-[calc(100vh-3.6rem)]  w-full  ">
-        <div className="h-full mx-auto overflow-y-auto lg:overflow-x-hidden pt-5 max-[350px]:px-5 px-[10%] flex flex-col gap-5 ">
-          <div
-            className="md:hidden relative -left-[10%] ">
-            <div className=" text-richblack-100 flex justify-center items-center  w-10 h-10 rounded-full bg-richblack-600 "
+      <div className="sm:h-[calc(100vh-3.6rem)]  w-full  ">
+        <div className=" mx-auto overflow-y-auto lg:overflow-x-hidden pt-5  px-[10%] flex flex-col gap-5 md:hidden relative -left-[6%] ">
+
+          <div className=" text-richblack-100 flex justify-center items-center  w-10 h-10 rounded-full bg-richblack-600 "
             onClick={() => dispatch(setOpen(!open))}
-            >
+          >
             <FaGreaterThan />
-            </div>
+
           </div>
-          <Outlet />
+        </div>
+        <div className=" p-5 max-[370px]:px-3">
+        <Outlet />
         </div>
       </div>
     </div>
