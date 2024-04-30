3eb8bf0b67b27cc900b0967805842a41bfdb6e76
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-04-30 10:54:52 +0530

    rupee sign add

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -201,7 +201,7 @@ export default function CourseDetails() {
             </div>
             <div className="flex w-full flex-col gap-4 border-y border-y-richblack-500 py-4 lg:hidden">
               <p className="space-x-3 pb-4 text-3xl font-semibold text-richblack-5">
-                Rs. {price}
+              ₹ {price}
               </p>
               <button className="yellowButton" 
               onClick={studentEnrolled.includes(user?._id)?
