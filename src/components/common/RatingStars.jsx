c0690771a2239108ee91b4b15b8e0b1098d5b3f6
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-08-24 17:33:06 +0530

    update rating star

--- src/components/common/RatingStars.jsx
+++ src/components/common/RatingStars.jsx
@@ -28,7 +28,7 @@ function RatingStars({ Review_Count, Star_Size }) {
       {[...new Array(starCount.half)].map((_, i) => {
         return <TiStarHalfOutline key={i} size={Star_Size || 20} />
       })}
-      {[...new Array(starCount.empty)].map((_, i) => {
+      {starCount?.empty>0 && [...new Array(starCount.empty)].map((_, i) => {
         return <TiStarOutline key={i} size={Star_Size || 20} />
       })}
     </div>
